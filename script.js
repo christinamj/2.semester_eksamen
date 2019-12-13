6236ec0c2d4fb32ebefd4a47dadedd7afd93f8ea
Christina Jørgensen <christina@Christinas-MacBook-Pro.local>
2019-12-13 01:19:23 +0100

    Burger fungerer mangler styling

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ document.addEventListener("DOMContentLoaded", start);
 function start() {
     hentNav();
     hentFooter();
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    document.querySelector("#menuknapluk").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    //            Burgermenuen er skjult på forsiden
+    document.querySelector("#burger").classList.toggle("hidden");
+
+    //            Hvis containerforside er synlig skal den skjules. Hvis den er skjult skal den vises
+    if (document.querySelector("#containerforside")) {
+        document.querySelector("#containerforside").classList.toggle("hidden");
+    }
+
+    //            Hvis menuknappen er synlig skal den skjules. Hvis den er skjult skal den vises
+    if (document.querySelector("#menuknap")) {
+        document.querySelector("#menuknap").classList.toggle("hidden");
+    }
 }
 async function hentNav() {
     //henter nav.html
